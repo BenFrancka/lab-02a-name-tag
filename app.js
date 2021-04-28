daa559c5ed09f03afda7234ae38054a3ff5df765
BenFrancka <benfrancka@gmail.com>
2021-04-28 15:34:28 -0700

    adds name/pronoun buttons in JS, adds beginning of font button

---
+++ app.js
@@ -0,0 +1,26 @@
+const userInput = document.getElementById("user-input");
+const nameButton = document.getElementById("name-button");
+const userName = document.getElementById("user-name");
+
+nameButton.addEventListener('click', () => {
+
+    userName.textContent = userInput.value;
+});
+
+
+const userPronounInput = document.getElementById("user-pronoun-input");
+const pronounButton = document.getElementById("pronoun-button");
+const userPronouns = document.getElementById("user-pronouns");
+
+pronounButton.addEventListener('click', () => {
+    userPronouns.textContent = userPronounInput.value;
+} );
+
+
+const fontChangeButton = document.getElementById("font-change-button");
+const helloFont = documnet.getElementById("hello-font");
+
+
+fontChangeBUtton.addEventListener('click', () => {
+    
+});
